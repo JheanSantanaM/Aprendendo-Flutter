54b1603005e70b528cf62c5a5616e66804ceea8e
Jhean Santana de Miranda <jheansmiranda@gmail.com>
2026-05-25 23:43:38 -0300

    Delete Conectados_2025-main directory

--- Conectados_2025-main/conectados/ex2.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-    stdout.write('Digite um numero: ');
-    String? parouimpar = stdin.readLineSync();
-    int n1 = int.parse(parouimpar!);
-    print(n1.isEven);
-}
--- Conectados_2025-main/conectados/ex3.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  int numero = 5;
-  print('Tabuada do $numero');
-  for (int i = 1; i <= 10; i++) {
-    int result = numero * i;
-    print('$numero * $i = $result');
-  }
-}
